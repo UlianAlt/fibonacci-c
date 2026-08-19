0dbc32f24f07646af5c47103e4c174fd9810bde3
UlianAlt <zpedro237732@gmail.com>
2026-08-19 20:03:11 -0300

    Fix Fibonacci function implementation

--- function_fibonacci.c
+++ function_fibonacci.c
@@ -1,9 +1,16 @@
-int fibonacci(int num){
+int fibonacci(int num) {
+    if (num <= 0) return 0;
+    if (num == 1) return 1;
+
+    int a = 0;
+    int b = 1;
     int result = 0;
 
-    for(int i = 0; i < num; i++){
-        result = (num-1) + (num-2);
+    for (int i = 2; i <= num; i++) {
+        result = a + b;
+        a = b;
+        b = result;
     }
-    
+
     return result;
 }
